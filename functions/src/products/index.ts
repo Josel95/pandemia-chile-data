de8980007c3a13eb740b9219882fc12b7fb52037
Josel95 <jose.lopezh95@gmail.com>
2021-04-01 23:29:54 -0300

    Product 19 download and process

--- functions/src/products/index.ts
+++ functions/src/products/index.ts
@@ -1,9 +1,12 @@
 import { Product1 } from './Product1'
+import { Product19 } from './Product19'
 
 export const getProductsData = async () => {
     const product1 = new Product1('output/producto1/Covid-19.csv')
+    const product19 = new Product19('output/producto19/CasosActivosPorComuna.csv')
 
     return {
-        product1: await product1.getData()
+        product1: await product1.getData(),
+        product19: await product19.getData(),
     }
 }   
